0fc67273f2cef9352cc7cb6ddf57fbe420f21e09
omicronrex <65692652+omicronrex@users.noreply.github.com>
2021-08-06 14:27:21 -0300

    add support for unicode file paths and names

--- dll/Buffer.cpp
+++ dll/Buffer.cpp
@@ -2,24 +2,25 @@
  * Copyright 2010-2013 Maarten Baert
  * maarten-baert@hotmail.com
  * http://www.maartenbaert.be/
- * 
+ *
  * This file is part of Http Dll 2.
- * 
+ *
  * Http Dll 2 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Http Dll 2 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Http Dll 2. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "Buffer.h"
+#include <windows.h>
 
 #include <cstdio>
 #include <cstdlib>
@@ -97,7 +98,12 @@ void Buffer::SetLength(unsigned int newlength) {
 
 bool Buffer::ReadFromFile(const char* filename) {
 	Clear();
-	FILE *f = fopen(filename, "rb");
+	int len = MultiByteToWideChar(CP_UTF8, 0, filename, -1, NULL, 0);
+    wchar_t *wname = (wchar_t *)malloc(len*2);
+    MultiByteToWideChar(CP_UTF8, 0, filename, -1, wname, len);
+	FILE *f;
+	f = _wfopen(wname, L"rb");
+    free(wname);
 	if(f == NULL) return false;
 	try {
 		fseek(f, 0, SEEK_END);
@@ -124,7 +130,12 @@ bool Buffer::ReadFromFile(const char* filename) {
 
 bool Buffer::ReadFromFilePart(const char* filename, unsigned int _pos, unsigned int _len) {
 	Clear();
-	FILE *f = fopen(filename, "rb");
+	int len = MultiByteToWideChar(CP_UTF8, 0, filename, -1, NULL, 0);
+    wchar_t *wname = (wchar_t *)malloc(len*2);
+    MultiByteToWideChar(CP_UTF8, 0, filename, -1, wname, len);
+	FILE *f;
+	f = _wfopen(wname, L"rb");
+    free(wname);
 	if(f == NULL) return false;
 	try {
 		if(fseek(f, _pos, SEEK_SET) != 0) {
@@ -147,8 +158,12 @@ bool Buffer::ReadFromFilePart(const char* filename, unsigned int _pos, unsigned 
 }
 
 bool Buffer::WriteToFile(const char* filename) {
+    int len = MultiByteToWideChar(CP_UTF8, 0, filename, -1, NULL, 0);
+    wchar_t *wname = (wchar_t *)malloc(len*2);
+    MultiByteToWideChar(CP_UTF8, 0, filename, -1, wname, len);
 	FILE *f;
-	f = fopen(filename, "wb");
+	f = _wfopen(wname, L"wb");
+    free(wname);
 	if(f == NULL) return false;
 	if(fwrite(data, 1, length, f) != length) {
 		fclose(f);
@@ -159,8 +174,12 @@ bool Buffer::WriteToFile(const char* filename) {
 }
 
 bool Buffer::AppendToFile(const char* filename) {
+	int len = MultiByteToWideChar(CP_UTF8, 0, filename, -1, NULL, 0);
+    wchar_t *wname = (wchar_t *)malloc(len*2);
+    MultiByteToWideChar(CP_UTF8, 0, filename, -1, wname, len);
 	FILE *f;
-	f = fopen(filename, "ab");
+	f = _wfopen(wname, L"ab");
+    free(wname);
 	if(f == NULL) return false;
 	if(fwrite(data, 1, length, f) != length) {
 		fclose(f);
