20535228d048aef71eb8c04c4fa095c3e53832a3
Floogle <18466542+skyfloogle@users.noreply.github.com>
2022-11-23 20:18:08 +0100

    buffer_make_pipe

---
+++ src/gm_pipe.cpp
@@ -0,0 +1,48 @@
+#include <thread>
+#include <sstream>
+
+#include "gm.h"
+#include "Buffer.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+// This function creates a named pipe which spits out the entire contents of the relevant buffer.
+// It returns the filename of the pipe.
+// This is useful for compressing models, as d3d_model_load only accepts filenames.
+// However, this won't work for any other such functions, as those have additional checks in place
+// using GetFileAttributesW to make sure the file exists, and calling that function with a pipe
+// is undefined behaviour that breaks everything.
+gmexport const char* buffer_make_pipe(double id) {
+	Buffer *b = gmdata.FindBuffer(gm_cast<unsigned int>(id));
+	if(b == NULL) return "";
+	volatile bool can_exit = false;
+	std::thread([b, &can_exit]() {
+		for (int i = 0;;i++) {
+			std::stringstream ss;
+			ss << "\\\\.\\pipe\\gm82netpipe." << i;
+			gmreturnstring = ss.str();
+			HANDLE pipe = CreateNamedPipeA(
+				gmreturnstring.c_str(),
+				PIPE_ACCESS_OUTBOUND | FILE_FLAG_FIRST_PIPE_INSTANCE,
+				PIPE_TYPE_BYTE,
+				10,
+				100,
+				100,
+				0,
+				NULL
+			);
+			if (pipe == INVALID_HANDLE_VALUE) continue;
+			can_exit = true;
+
+			ConnectNamedPipe(pipe, NULL);
+			ConnectNamedPipe(pipe, NULL);
+			DWORD bytes_written;
+			WriteFile(pipe, b->GetData(), b->GetLength(), &bytes_written, NULL);
+			CloseHandle(pipe);
+			break;
+		}
+	}).detach();
+	while (!can_exit) Sleep(0);
+	return gmreturnstring.c_str();
+}
