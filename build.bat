d32a89ba9253a095891c99a9d7fadf5c88312212
omicronrex <65692652+omicronrex@users.noreply.github.com>
2023-08-15 17:44:11 -0300

    fix upx

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+del build\src\release\gm82net.dll
+
 cmake -B build -T v141_xp -A Win32 -DINSTALL_GEX=ON && cmake --build build --config Release
 
 pause
--- build64.bat
+++ build64.bat
@@ -1,3 +1,5 @@
+del build\src\release\gm82core.dll
+
 cmake -B build -A Win32 -DNO_GEX=ON && cmake --build build --config Release
 copy build\src\Release\gm82net.dll .
 pause
