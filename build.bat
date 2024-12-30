b6255ba73f0c52a6f7016a9fc4fd6e1621738fcd
omicronrex <65692652+omicronrex@users.noreply.github.com>
2024-12-30 00:11:14 -0300

    add extension crosstalk support

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 del build\src\release\gm82net.dll
 
-cmake -B build -T v141_xp -A Win32 -DINSTALL_GEX=ON && cmake --build build --config Release
+cmake -B build -A Win32 -DINSTALL_GEX=ON && cmake --build build --config Release
 
 pause
---
+++ src/gm82net.gml
@@ -0,0 +1,5 @@
+#define __gm82net_init
+    globalvar __gm82net_version;
+    __gm82net_version=131
+//
+//
